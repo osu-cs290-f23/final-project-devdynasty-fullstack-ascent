58e8e35a4f4bbe5c15f8f161c387a5637da976ec
ArtofBugs <74070945+ArtofBugs@users.noreply.github.com>
2023-11-28 00:20:57 -0800

    Add gltf loading and orbit controls

--- static/job-apps.js
+++ static/job-apps.js
@@ -1,35 +1,60 @@
 import * as THREE from 'three';
+import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js'
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
 
 
+// Global variables
 const FOV_deg = 75; // field of view in degrees
 const aspect_ratio = window.innerWidth / window.innerHeight;
 const near_clip = 0.1;
 const far_clip = 1000;
 
+// Setup
 const scene = new THREE.Scene();
+scene.background = new THREE.Color(0xff0000);
 const camera = new THREE.PerspectiveCamera(FOV_deg, aspect_ratio, near_clip, far_clip);
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.prepend(renderer.domElement);  // this is a <canvas> element
 
+/*
+// Add cube
 const geometry = new THREE.BoxGeometry(1, 1, 1);
 const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
 const cube = new THREE.Mesh(geometry, material);
 scene.add(cube);
+*/
 
-camera.position.x = 0;
-camera.position.y = 0;
-camera.position.z = 5;
+// Add light
+var alight = new THREE.AmbientLight(0x404040, 100);
+scene.add(alight);
+
+// Add computers
+const loader = new GLTFLoader();
+loader.load('scene.gltf', function (gltf) {
+    var stuff = gltf.scene.children[0];
+    stuff.scale.set(200, 200, 200);
+    scene.add(gltf.scene);
+}, undefined, function (error) {
+    console.error(error);
+})
+
+const controls = new OrbitControls(camera, renderer.domElement);
+camera.position.set(0, 20, 100);
+controls.update();
 
 function animate() {
     requestAnimationFrame(animate);
+    controls.update();
 
     // animation here
+    /*
     cube.rotation.x += 5;
     cube.rotation.y += 5;
-
+    */
 
     renderer.render(scene, camera);
 }
 
 animate();
+
