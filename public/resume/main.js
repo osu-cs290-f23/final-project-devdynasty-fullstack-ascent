79721623ba67066a5326982292916671fc3f9c61
ArtofBugs <74070945+ArtofBugs@users.noreply.github.com>
2023-12-13 01:36:19 -0800

    Add deepti's updated resume page files

--- public/resume/main.js
+++ public/resume/main.js
@@ -4,20 +4,25 @@ let skills2Count = 0;
 let skills3Count = 0;
 
 // Arrays for good and bad skills
-const goodSkills1 = ["node.js", "python", "javascript", "html & css", "REACT", "SQL", "Database design", "Express.js", "Apache", "AWS", "Docker"];
-const badSkills1 = ["Microsoft Excel", "C#", "Data Entry", "Customer Service", "MASM", "MATLAB"];
+const goodSkills1 = ["node.js", "python", "javascript", "html & css", "REACT", "SQL", "Database design", "Express.js", "AWS", "Docker"];
+const badSkills1 = ["Microsoft Excel", "Graphic Design", "Data Entry","Apache", "MASM", "C#", "Customer Service", "MATLAB"];
 
-const goodSkills2 = ["Project Lead for E-commerce Overhaul", "Software Engineering Intern at ABC Innovations", "IT Project Manager at Global Solutions", "Back-End Developer at DataMinds Co.", "Front-End Developer at TechCraft Studios"];
-const badSkills2 = ["Data Entry Intern at FedEx", "Office Assistant at Mary's Coding Camp", "Quality Insurance Intern at Intel", "Cybersecurity Lead at Ebay"];
 
-const goodSkills3 = ["Secure Blogging Platform with Multi-Factor Authentication", "AI-Powered Personal Portfolio Optimization", "Blockchain-Based Task Management Application"];
-const badSkills3 = ["Portfolio Website", "To Do List App", "Social Media Metrics Dashboard"];
+const goodSkills2 = ["Software Engineering Intern at ABC Innovations", "IT Project Manager at Global Solutions", "Front-End Developer Intern at TechCraft Studios", "Teaching Assistant at Mary Coding Camp", "Quality Assurance Part-Time Intern at Intel", "Google Cybersecurity Certification"];
+const badSkills2 = ["Data Entry Summer Job at FedEx", "Volunteer for E-commerce Overhaul", "Barista at Starbucks"];
+
+
+const goodSkills3 = ["Secure Blogging Platform with Multi-Factor Authentication with Firebase", "Portfolio Website using React", "Social Media Metrics Dashboard","Metadata Search Application using Spotify and Genius API" ];
+const badSkills3 = ["Customer Service Spreadsheet", "To Do List App", "Entrepreneurial Mindset Vlog Channel on Youtube"];
+
 
 // Point system
 const pointSystem = {
     goodSkill: 1, // Points for each good skill
     badSkill: 0, // Points for each bad skill
+    //okaySkill: 0.5, // Points for each okay skill
 };
+
 
 
 function allowDrop(event) {
@@ -33,13 +38,13 @@ function drop(event, targetId) {
     var data = event.dataTransfer.getData("text");
 
     // Check the targetId to determine the destination box
-    if (targetId === "otherSkillsList" && skillsCount < 7) {
+    if (targetId === "otherSkillsList" && skillsCount < 9) {
         addToBox(data, targetId);
         skillsCount++;
     } else if (targetId === "workExperienceList" && skills2Count < 6) {
         addToBox(data, targetId);
         skills2Count++;
-    } else if (targetId === "projectsList" && skills3Count < 4) {
+    } else if (targetId === "projectsList" && skills3Count < 5) {
         addToBox(data, targetId);
         skills3Count++;
     }
@@ -71,30 +76,25 @@ function addToBox(data, targetId) {
 }
 
 
-
-
 function calculatePoints(goodSkills, badSkills, userSkills) {
-    let points = 0;
+    let overallPoints = 0;
 
     userSkills.forEach(skill => {
-        const lowercaseSkill = skill.toLowerCase();  // Convert skill to lowercase for case-insensitive comparison
+        const lowercaseSkill = skill.toLowerCase();
         if (goodSkills.map(s => s.toLowerCase()).includes(lowercaseSkill)) {
-            points += pointSystem.goodSkill;
+            overallPoints += pointSystem.goodSkill;
         } else if (badSkills.map(s => s.toLowerCase()).includes(lowercaseSkill)) {
-            points += pointSystem.badSkill;
+            // No points deducted for bad skills
+
         } else {
             console.log(`Unknown skill: ${skill}`);
         }
     });
 
-    return points;
+    return overallPoints;
 }
 
-
-// ... (existing code)
-
 function redirectToNextPage(hasInterviewOffer) {
-    // Store the result in sessionStorage to retrieve it on nextpage.html
     sessionStorage.setItem("interviewResult", hasInterviewOffer ? "Congratulations! You got an interview offer!" : "Sorry, you did not qualify for an interview offer.");
     window.location.href = "nextpage.html";
 }
@@ -104,18 +104,14 @@ function checkInterviewOffer() {
     const otherSkills = getSkillsFromList("otherSkillsList");
     const projectsSkills = getSkillsFromList("projectsList");
 
-    // Calculate points for each category
-    const points1 = calculatePoints(goodSkills1, badSkills1, otherSkills);
-    const points2 = calculatePoints(goodSkills2, badSkills2, workExperienceSkills);
-    const points3 = calculatePoints(goodSkills3, badSkills3, projectsSkills);
+    const overallPoints1 = calculatePoints(goodSkills1, badSkills1,  otherSkills);
+    const overallPoints2 = calculatePoints(goodSkills2, badSkills2,  workExperienceSkills);
+    const overallPoints3 = calculatePoints(goodSkills3, badSkills3,  projectsSkills);
 
-    // Check if the user meets the criteria for an interview offer
-    const hasInterviewOffer = points1 >= 3 && points2 >= 3 && points3 >= 2;
+    const hasInterviewOffer = overallPoints1 >= 3 && overallPoints2 >= 3 && overallPoints3 >= 3;
 
-    // Redirect to nextpage.html without displaying an alert
     redirectToNextPage(hasInterviewOffer);
 }
-
 
 
 function getSkillsFromList(listId) {
